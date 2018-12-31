2e6ed923cbb129a84fa36b8b0a7964fca2d6aedf
omar5552018 <omar5552016@hotmail.com>
2018-12-31 15:09:21 +0300

    Done ListView

--- app/src/main/java/com/example/omaralsaedi/store/Controller/MainActivity.kt
+++ app/src/main/java/com/example/omaralsaedi/store/Controller/MainActivity.kt
@@ -3,6 +3,7 @@
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.omaralsaedi.store.Adapter.CategoryAdapter
 import com.example.omaralsaedi.store.Model.Category
 import com.example.omaralsaedi.store.R
 import com.example.omaralsaedi.store.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoryListView.adapter = adapter
     }
